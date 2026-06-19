530e72cb02f789961fb9a550448036f5c393cdcf
SarveshwaraReddy <sarveshwarareddy9@gmail.com>
2026-06-19 10:06:11 +0530

    feat: create Django company portal project

---
+++ employees/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'employees/home.html')
+
+
+def about(request):
+    return render(request, 'employees/about.html')
+
+
+def contact(request):
+    return render(request, 'employees/contact.html')
